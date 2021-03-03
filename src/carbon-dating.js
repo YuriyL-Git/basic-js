74372708d873be9f6f55ac82d39a429a5d2edd23
Yuriy Lipchey <xaosmmm@gmail.com>
2021-03-03 14:37:52 +0200

    feat: add solution for dream-team and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity !== 'string') return false
+    let input = parseFloat(sampleActivity)
+    if (input<=0 || input>=15) return false
+    if (!input) return false;
+    return Math.ceil((Math.log(MODERN_ACTIVITY / input)) / (Math.log(2) / HALF_LIFE_PERIOD));
 };
+
+let inp = 11;
+let p = 4
+
+
+
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) return false
+    let out = ''
+    members.forEach(m => {
+            if (typeof m === 'string') out += m.trim()[0]
+        }
+    )
+    return out.toUpperCase().split('').sort().join('')
 };
