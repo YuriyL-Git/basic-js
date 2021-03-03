e9c8618b1ef60dc6c59c9adf092e5a2a4211d844
Yuriy Lipchey <xaosmmm@gmail.com>
2021-03-03 14:44:03 +0200

    refactor: change type of output from string to array to eliminate call of split function

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,9 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
     if (!Array.isArray(members)) return false
-    let out = ''
+    let out = []
     members.forEach(m => {
-            if (typeof m === 'string') out += m.trim()[0]
-        })
-    return out.toUpperCase().split('').sort().join('')
+        if (typeof m === 'string') out.push(m.trim().charAt(0).toUpperCase())
+    })
+    return out.sort().join('')
 };
