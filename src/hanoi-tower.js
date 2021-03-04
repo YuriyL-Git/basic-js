a05d2608451c665fee4d3d5291dfd64b8ec46834
Yuriy Lipchey <xaosmmm@gmail.com>
2021-03-04 16:04:06 +0200

    refactor: remove unnecessary object names

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -4,5 +4,5 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     const turns = Math.pow(2, disksNumber) - 1
     const seconds = Math.floor(3600 * turns / turnsSpeed)
 
-    return {turns:turns, seconds:seconds}
+    return {turns, seconds}
 };
