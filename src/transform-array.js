81891e615b8c1741dd684396cb372be4107fb615
Yuriy Lipchey <xaosmmm@gmail.com>
2021-03-05 12:39:18 +0200

    add solution for transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =
+    function transform(arr) {
+      if (!Array.isArray(arr)) throw new Error
+
+      const res = []
+      let lastElement = null
+      const sequences = ['--double-prev', '--discard-prev', '--discard-next', '--double-next', 'done']
+
+      arr.forEach(el => {
+        switch (el) {
+
+          case '--double-prev':
+            if (sequences.includes(lastElement)) {
+              lastElement = null
+              break
+            }
+            if (res.length) res.push(lastElement)
+            break
+
+          case '--discard-prev':
+            if (sequences.includes(lastElement)) break
+            res.pop()
+            break
+
+          case '--discard-next':
+          case '--double-next':
+            lastElement = el
+            break
+
+          default:
+            if (lastElement === '--discard-next') {
+              lastElement = 'done'
+              break
+            }
+            if (lastElement === '--double-next') res.push(el)
+            lastElement = el
+            res.push(el)
+        }
+      })
+      return res
+    };
+
+
