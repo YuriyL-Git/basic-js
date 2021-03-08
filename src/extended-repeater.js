0ef5fa688394c093e4ea3ac8a6e590a72084f0ac
Yuriy Lipchey <xaosmmm@gmail.com>
2021-03-08 15:54:35 +0200

    feat: add solution for extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports =
+    function repeater(str, options) {
+      options.separator = options.separator || '+'
+      options.additionSeparator = options.additionSeparator || '|'
+      options.repeatTimes = options.repeatTimes || 1
+      options.additionRepeatTimes = options.additionRepeatTimes || 1
+
+      if (typeof options.addition !== 'undefined') {
+        options.addition = String(options.addition)
+      } else {
+        options.addition = ''
+      }
+
+      let res = String(str)
+      res += (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes)
+      res = res.substr(0, res.lastIndexOf(options.additionSeparator))
+      res = (res + options.separator).repeat(options.repeatTimes)
+      res = res.substr(0, res.lastIndexOf(options.separator))
+
+      return res
+    }
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+module.exports =
+    class DepthCalculator {
+      calculateDepth(array) {
+        if (!Array.isArray(array)) return 0
+        if ((array).length === 0) return 1
+        return Math.max(...array.map(a => this.calculateDepth(a))) + 1
+      }
+    }
